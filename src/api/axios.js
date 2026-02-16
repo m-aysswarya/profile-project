9566ef9c1d34c51f3dbccd65f6f2d98815438cc8
aysswarya-1 <a.aysswarya@gmail.com>
2026-02-16 16:41:21 +0530

    Added Url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const apiUrl = import.meta.env.VITE_BASE_URL;
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: apiUrl,
 });
 
 export default API;
